6a36af60db88f00b3639d387670e77273481b208
LOWLIGHT_AKBOTA <fhjj3590@gmail.com>
2024-10-17 18:53:27 +0500

    Initial commit

---
+++ educa/educa/settings/prod.py
@@ -0,0 +1,24 @@
+from .base import *
+import os 
+
+DEBUG = False
+
+ADMINS = [
+    ('Akbota S','fhjj3590@gmail.com'),
+]
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+ }
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
